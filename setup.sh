3409924befdbc09cf7da7173b1b2dc71484d1653
Zarko Petrov <95506092+Zaryooo@users.noreply.github.com>
2024-05-07 09:17:02 +0000

    Add drupal static generator.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ composer require drush/drush
 vendor/bin/drush si --db-url=sqlite://drupal:drupal@127.0.0.1:3306/drupal
 vendor/bin/drush upwd admin 123
 vendor/bin/drush generate phpstorm-metadata
+composer require 'drupal/static_generator:^2.0@alpha'
 
 echo 'export PATH="./vendor/bin:$PATH"' >> ~/.bashrc
 export PATH="./vendor/bin:$PATH
